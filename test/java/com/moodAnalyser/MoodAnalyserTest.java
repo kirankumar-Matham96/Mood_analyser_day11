4d976715fe3f22de434c8c86f7b2fb15956dff27
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-02 20:53:35 +0530

    ADD: test cade added to return HAPPY when passed 'I am in Any Mood'

--- test/java/com/moodAnalyser/MoodAnalyserTest.java
+++ test/java/com/moodAnalyser/MoodAnalyserTest.java
@@ -6,10 +6,18 @@ import org.junit.Test;
 public class MoodAnalyserTest
 {
     @Test
-    public void testMoodAnalyser()
+    public void givenIAmInSadMood_returnSad()
     {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.alyseMood("I am in Sad Mood");
         Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void givenIAmInAnyMood_returnHappy()
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.alyseMood("I am In Any Mood");
+        Assert.assertEquals("HAPPY",mood);
     }
 }
